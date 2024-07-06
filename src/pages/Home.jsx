5d92e9e40624c139cca255fccacd2af3e7415b2c
Abhishek Santra <abhisheksantra28@gmail.com>
2024-07-06 18:04:52 +0530

    Fixes merge conflicts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,20 @@ import HeroSection from "../components/Home/HeroSection";
 import Ecosystem from "../components/Ecosystem";
 import GeminiSection from "../components/GeminiSection";
 import Features from "../components/Home/Features";
-<<<<<<< HEAD
-import {Vortex} from "../components/ui/vortex";
-import StarBackground from "../components/ui/StarBackground";
-=======
-// import {Vortex} from "../components/ui/vortex";
 
->>>>>>> 4fbebb183f07d61400c6601384a8c4b157aac6ff
+import { Vortex } from "../components/ui/vortex";
+import StarBackground from "../components/ui/StarBackground";
+
 function Home() {
   return (
     <div className="text-richblack-5">
       {/* <StarBackground className="" /> */}
       {/* <Vortex> */}
-        <HeroSection />
+      <HeroSection />
       {/* </Vortex> */}
       <Features />
       <Ecosystem />
       <GeminiSection />
-  
     </div>
   );
 }
