857721c2130009f0c81c08561ae2aab0c48c7094
Abhishek Santra <abhisheksantra28@gmail.com>
2024-07-06 10:35:58 +0530

    Add Ecosystem component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import HeroSection from "../components/Home/HeroSection"
+import Ecosystem from "../components/Ecosystem"
 
 
 function Home() {
   return (
     <div className="text-richblack-5">
       <HeroSection />
+      <Ecosystem />
+      
     </div>
   )
 }
