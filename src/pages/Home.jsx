e5c4d4b187838caa1be9271ae71d8ef8e16511c4
RAHUL KARMAKAR <karmakarrahul503@gmail.com>
2024-07-06 17:43:28 +0530

    push partial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import Ecosystem from "../components/Ecosystem";
 import GeminiSection from "../components/GeminiSection";
 import Features from "../components/Home/Features";
 import {Vortex} from "../components/ui/vortex";
+import StarBackground from "../components/ui/StarBackground";
 function Home() {
   return (
     <div className="text-richblack-5">
+      {/* <StarBackground className="" /> */}
       {/* <Vortex> */}
         <HeroSection />
       {/* </Vortex> */}
