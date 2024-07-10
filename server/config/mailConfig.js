e3210710cfe2109b11d3dd29a9794a7d257e934b
Abhishek Santra <abhisheksantra28@gmail.com>
2024-07-10 17:17:27 +0530

    Modified mail config file

--- server/config/mailConfig.js
+++ server/config/mailConfig.js
@@ -5,6 +5,8 @@ require("dotenv").config()
 
 exports.transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
+    // port: 587, // or 465 for SSL
+    // secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
